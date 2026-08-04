61166f42e7f06d2caa5f9b6af9f343cc085f6148
이지플로우 <dltjdrms320@gmail.com>
2026-08-04 15:50:48 +0900

    style: 소속 기관으로 명칭 변경 및 전체 역할 더미 데이터 5건(장학사, 멘토 등) 추가

---
+++ update_filters.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const files = ['서버관리자_회원관리.html', '서버관리자_역할관리.html'];
+
+files.forEach(file => {
+    let content = fs.readFileSync(file, 'utf8');
+
+    // 1. Update filter options
+    const oldOptions = `<option>전체 역할</option>
+<option>총괄 관리자</option>
+<option>학교 관리자</option>
+<option>교사</option>`;
+    
+    const newOptions = `<option>전체 역할</option>
+<option>서버 관리자</option>
+<option>학교 관리자</option>
+<option>장학사</option>
+<option>멘토</option>
+<option>교사</option>`;
+
+    content = content.replace(oldOptions, newOptions);
+
+    // If there is any remaining "총괄 관리자" string, we replace it with "서버 관리자"
+    content = content.replace(/총괄 관리자/g, '서버 관리자');
+
+    fs.writeFileSync(file, content, 'utf8');
+});
+
+console.log('Updated filters and renamed roles');
